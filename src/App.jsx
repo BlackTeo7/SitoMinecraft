2cef236e84e23cea2b6bc49f672659363332fc45
Matteo Bronzi <matteo.bronzi.07@gmail.com>
2026-08-01 01:15:03 +0200

    Primo commit

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import {useEffect,useState} from "react";
+import "./App.css";
+
+
+function App(){
+
+const [server,setServer]=useState(null);
+
+
+async function check(){
+
+    const res = await fetch(
+        "http://localhost:3001/status"
+    );
+
+    const data = await res.json();
+
+    setServer(data);
+
+}
+
+
+
+useEffect(()=>{
+
+    check();
+
+    const timer=setInterval(
+        check,
+        10000
+    );
+
+
+    return()=>clearInterval(timer);
+
+
+},[]);
+
+
+
+return (
+
+<div className="page">
+
+<div className="card">
+
+
+<h1>
+⛏️ Minecraft Server
+</h1>
+
+
+{
+!server ?
+
+<h2>
+🔄 Controllo...
+</h2>
+
+
+:
+
+server.online ?
+
+
+<>
+
+<h2 className="online">
+🟢 ONLINE
+</h2>
+
+
+<h3>
+👥 {server.players.online}/{server.players.max}
+</h3>
+
+
+<p>
+Versione:
+{server.version}
+</p>
+
+
+<p>
+{server.motd}
+</p>
+
+</>
+
+
+:
+
+<h2 className="offline">
+🔴 OFFLINE
+</h2>
+
+}
+
+
+</div>
+
+</div>
+
+
+)
+
+
+}
+
+
+export default App;
